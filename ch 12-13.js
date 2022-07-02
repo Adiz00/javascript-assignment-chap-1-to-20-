fbf901f2a88008893c1a8766df6d9dbaac60a71b
M ADiL <101953839+Adiz00@users.noreply.github.com>
2022-07-02 05:34:14 -0700

    Add files via upload

---
+++ ch 12-13.js
@@ -0,0 +1,62 @@
+// Q1
+
+// var value=prompt("Enter value");
+// if(value.charCodeAt(0) > 64 && value.charCodeAt(0) < 90 ){
+// alert("it is uppercase");
+// }
+// else if(value.charCodeAt(0) > 96 && value.charCodeAt(0) < 123){
+//     alert("it is lowercase");
+// }
+// else if(value.charCodeAt(0) > 47 && value.charCodeAt(0) < 58){
+//     alert("it is number");
+// }
+// else{
+//     alert("it is sign!");
+// }
+// =================================================
+
+// Q2
+
+// var val1=prompt("enter value 1");
+// var val2=prompt("enter value 2");
+
+// if(val1 > val2){
+//     alert("val 1 is greater.");
+// }
+// else if(val2 > val1){
+//     alert("val 2 is greater.");
+// }
+// else{
+//     alert("both are equal");
+// }
+// =================================================
+
+// Q3
+
+// var val1=prompt("enter num");
+
+
+// if(val1 > 0){
+//     alert("positive.");
+// }
+// else if(val1 < 0){
+//     alert("negative.");
+// }
+// else{
+//     alert("it is zero");
+// }
+
+// =================================================
+
+// Q4
+
+// var char=prompt("enter character");
+
+
+// if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u" ){
+//    alert("vowel");
+// }
+// else{
+//     alert("consunant");
+// }
+// ===============================================
